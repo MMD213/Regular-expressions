393873472d841fb8c20a62c6fd575a8e3f57a9c5
Макаров Михаил <58745072+MMD213@users.noreply.github.com>
2022-09-27 18:58:56 +0300

    Update Main.java

--- Dz1/Main.java
+++ Dz1/Main.java
@@ -8,8 +8,7 @@ public class Main {
         Pattern pattern = Pattern.compile("(([0-1]\\d|2\\[0-4])\\:\\d\\d)");
         Matcher matcher = pattern.matcher("Завтрак в 09:00, ужин в 18:00, обед в 13:00, обед в 121:00");
         while (matcher.find()){
-            System.out.println(matcher.group() + " ");
+            System.out.println(matcher.group());
         }
-        System.out.println("");
     }
 }

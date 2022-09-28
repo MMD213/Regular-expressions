3279e04bfd15030d4c445bd8d61b9fb89676031c
Макаров Михаил <58745072+MMD213@users.noreply.github.com>
2022-09-28 15:58:48 +0300

    Add files via upload

--- Dz1/Main.java
+++ Dz1/Main.java
@@ -1,14 +1,6 @@
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class Main {
     public static void main(String[] args) {
-
-
-        Pattern pattern = Pattern.compile("(([0-1]\\d|2\\[0-4])\\:[0-5]\\d)");
-        Matcher matcher = pattern.matcher("Завтрак в 09:00, ужин в 18:00, обед в 13:00, обед в 121:00");
-        while (matcher.find()){
-            System.out.println(matcher.group());
-        }
+        Met met = new Met();
+        met.Cag();
     }
 }
---
+++ Dz1/Met.java
@@ -0,0 +1,13 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+  public class Met {
+        void Cag() {
+
+            Pattern pattern = Pattern.compile("(([0-1]\\d|2\\[0-4])\\:[0-5]\\d)");
+            Matcher matcher = pattern.matcher("Завтрак в 09:00, ужин в 18:00, обед в 13:00, обед в 121:00");
+            while (matcher.find()){
+                System.out.println(matcher.group());
+            }
+        }
+  }
+

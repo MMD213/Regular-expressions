ad6dc9d7a7244e1de1fad83e130810ca046daa74
Макаров Михаил <58745072+MMD213@users.noreply.github.com>
2022-10-17 17:46:43 +0300

    Update Met.java

--- Dz1/Met.java
+++ Dz1/Met.java
@@ -1,7 +1,7 @@
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
   public class Met {
-        void Cag() {
+        void Search() {
 
             Pattern pattern = Pattern.compile("(([0-1]\\d|2\\[0-4])\\:[0-5]\\d)");
             Matcher matcher = pattern.matcher("Завтрак в 09:00, ужин в 18:00, обед в 13:00, обед в 121:00");
